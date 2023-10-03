52ee0efe8e97b5e76fa3d08b1162c00f97d35a1f
Tom Sproull <64221818+sprouller@users.noreply.github.com>
2023-10-03 22:25:11 +0100

    add maturity logic

--- maturity/sharesave/index.js
+++ maturity/sharesave/index.js
@@ -1,49 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   // IDs of parameters
+  const OPTIONS_PRICE_TEXT = 'optionPrice';
   const SAVINGS_INPUT = 'savings';
-  const INITIAL_TEXT = 'initial';
+  const TOTAL_SAVED_TEXT = 'totalSaved';
   const NUMBER_OF_SHARES_TEXT = 'numberOfShares';
-  const SHARE_TEXT = 'sharePrice';
-  const VALUE_TEXT = 'value';
+  const SHARE_PRICE_TEXT = 'sharePrice';
+  const VALUE_TEXT = 'initialShareValue';
+  const INITIAL_PROFIT_TEXT = 'initialProfit';
   const SLIDER = 'increase';
   const CHANGE_TEXT = 'change';
-  const PROFIT_TEXT = 'profit';
-  const SLIDER_VAL = 'sliderVal';
-  const OPTION_PRICE = 'optionPrice';
+  const TOTAL_PROFIT_TEXT = 'profit';
 
   // Get elements based on IDs
+  const nwOptionPriceDisplay = document.getElementById(OPTIONS_PRICE_TEXT);
   const savings = document.getElementById(SAVINGS_INPUT);
-  const initial = document.getElementById(INITIAL_TEXT);
+  const totalSaved = document.getElementById(TOTAL_SAVED_TEXT);
   const numberOfShares = document.getElementById(NUMBER_OF_SHARES_TEXT);
-  const sharePrice = document.getElementById(SHARE_TEXT);
-  const value = document.getElementById(VALUE_TEXT);
+  const sharePrice = document.getElementById(SHARE_PRICE_TEXT);
+  const initialValue = document.getElementById(VALUE_TEXT);
+  const initialProfit = document.getElementById(INITIAL_PROFIT_TEXT);
   const increase = document.getElementById(SLIDER);
   const change = document.getElementById(CHANGE_TEXT);
-  const profit = document.getElementById(PROFIT_TEXT);
-  const sliderVal = document.getElementById(SLIDER_VAL);
-  const optionPrice = document.getElementById(OPTION_PRICE);
+  const profit = document.getElementById(TOTAL_PROFIT_TEXT);
   const periodButtons = document.querySelectorAll('input[name="period"]');
-  //const profitOver3k = document.getElementById(PROFIT_OVER_3K);
 
   // API URL
   const apiUrl = 'https://alpha-pulse.vercel.app/api/eod?id=nwg';
 
   // Function to round share price
   const roundSharePrice = (x) => {
-      return new Intl.NumberFormat('en-IN', {
+      return new Intl.NumberFormat('en-GB', {
           style: 'currency',
-          currency: 'INR',
+          currency: 'GBP',
           minimumFractionDigits: 2,
           maximumFractionDigits: 2,
       }).format(x);
   }
 
+  // Function to round numbers
+  const roundMe = (x) => {
+    return new Intl.NumberFormat().format(x);
+}
+
   // Initialize the live share price as null
   let liveSharePrice = null;
   let nwOptionPrice = 1.88;  // default value
   let savingPeriod = 3;
-  let conversionRate = 106;
 
   // Fetch the share price from the API
   const getSharePrice = async () => {
@@ -59,15 +62,15 @@ document.addEventListener('DOMContentLoaded', () => {
       }
   };
 
-    // Remove comma and pound sign from input
-    const stripNumber = (textNumber) => {
-      return textNumber.replace(/[₹,]/g, "");
-    }
-    
-    // Add commas and pound sign to input
-    const addSymbolsToInput = (numberInput) => {
-      return numberInput.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
+  // Remove comma and pound sign from input
+  const stripNumber = (textNumber) => {
+    return textNumber.replace(/[£,]/g, "");
+  }
+  
+  // Add commas and pound sign to input
+  const addSymbolsToInput = (numberInput) => {
+    return numberInput.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
 
   // Initialize the page once the share price is fetched
   const initPage = async () => {
@@ -75,20 +78,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Determine the value of nwOptionPrice here after liveSharePrice is set
     if (liveSharePrice !== null) {
-      convertedSharePrice = liveSharePrice * conversionRate;
-      nwOptionPrice = convertedSharePrice * 0.8;
+      nwOptionPrice = liveSharePrice * 0.8;
+      nwOptionPriceDisplay.textContent = roundMe(nwOptionPrice);
     }
 
   // Update the live share price on the page
-  sharePrice.textContent = roundSharePrice(convertedSharePrice);
-  // Update the live option price on the page
-  optionPrice.textContent = roundSharePrice(nwOptionPrice);
-
+  sharePrice.textContent = roundSharePrice(liveSharePrice);
       
       // Add event listeners here if they depend on the fetched share price
       savings.addEventListener('input', () => {
         savings.value = addSymbolsToInput(savings.value);
-        savings.value = "₹" + savings.value;
+        savings.value = "£" + savings.value;
 
         // Set new constant to value of unformmated input
         let formattedSavingsValue = stripNumber(savings.value);
@@ -106,8 +106,8 @@ document.addEventListener('DOMContentLoaded', () => {
       periodButtons.forEach(radio => {
         radio.addEventListener('change', function() {
           savingPeriod = parseInt(this.value, 10);
-          const savingsVal = stripNumber(savings.value);
-          const increaseVal = increase.value;
+          let savingsVal = stripNumber(savings.value);
+          let increaseVal = increase.value;
           updateTotals(savingsVal, increaseVal);
         });
     });
@@ -115,29 +115,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const updateTotals = (savingsVal, increaseVal) => {
       const roundMeCurrency = (x) => {
-          return new Intl.NumberFormat('en-IN', {
+          return new Intl.NumberFormat('en-GB', {
               style: 'currency',
-              currency: 'INR',
+              currency: 'GBP',
               minimumFractionDigits: 0,
               maximumFractionDigits: 0,
           }).format(x);
       }
 
-      const roundMe = (x) => {
-          return new Intl.NumberFormat().format(x);
-      }
+      let updatedTotalSaved = savingsVal * 12 * savingPeriod;
+      totalSaved.textContent = roundMeCurrency(updatedTotalSaved);
 
-      const updateInitial = savingsVal * 12 * savingPeriod;
-      const updateNumber = Math.floor(updateInitial / nwOptionPrice);
-      const updateValue = Math.round(updateNumber * convertedSharePrice);
-      const updateChange = updateValue * (1 + (increaseVal / 100));
-      const updateProfit = updateChange - updateInitial;
+      let updatedNumberOfShares = Math.floor(updatedTotalSaved / nwOptionPrice);
+      numberOfShares.textContent = roundMe(updatedNumberOfShares);
 
-      initial.textContent = roundMeCurrency(updateInitial);
-      numberOfShares.textContent = roundMe(updateNumber);
-      value.textContent = roundMeCurrency(updateValue);
-      change.textContent = roundMeCurrency(updateChange);
-      profit.textContent = roundMeCurrency(updateProfit);
+      let updatedInitialValue = updatedNumberOfShares * liveSharePrice;
+      initialValue.textContent = roundMe(updatedNumberOfShares);
+
+      let updatedInitialProfit = updatedInitialValue - updatedTotalSaved;
+      initialProfit.textContent = roundMeCurrency(updatedInitialProfit);
+
+      let updatedChange = updatedInitialValue * (1 + (increaseVal / 100));
+      change.textContent = roundMeCurrency(updatedChange);
+
+      let updatedProfit = updatedChange - updatedTotalSaved;
+      profit.textContent = roundMeCurrency(updatedProfit);
+
   }
 
   // Initialize the page
